866cf325fb243604da6a99446701e0f918710af1
OhhBuddie <developer@ohhbuddie.com>
2025-04-11 19:15:01 +0530

    pay url

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -195,9 +195,11 @@ class CheckoutController extends Controller
             ];
 
             // PayU payment URL (live or test)
-            $payuUrl = config('payu.mode') === 'live'
-                ? 'https://secure.payu.in/_payment'
-                : 'https://test.payu.in/_payment';
+            // $payuUrl = config('payu.mode') === 'live'
+            //     ? 'https://secure.payu.in/_payment'
+            //     : 'https://test.payu.in/_payment';
+
+            $payuUrl = 'https://secure.payu.in/_payment';
 
             return response()->json([
                 'payment_url' => $payuUrl,

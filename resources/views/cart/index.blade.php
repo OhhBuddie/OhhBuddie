de7925be20fa004da4f6d6befe8a5c2bc5cc5058
OhhBuddie <developer@ohhbuddie.com>
2025-04-24 17:19:28 +0530

    script <

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1582,11 +1582,6 @@
 
 
 
-
-
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         function showToast(message, type = 'primary') {
@@ -1913,8 +1908,7 @@
     <!--<script src="https://kit.fontawesome.com/YOUR_KIT_CODE.js" crossorigin="anonymous">
         < /script-->
 
-        <
-        script src = "https://code.jquery.com/jquery-3.6.0.min.js" >
+    
     </script>
     <script>
         $(document).ready(function() {
